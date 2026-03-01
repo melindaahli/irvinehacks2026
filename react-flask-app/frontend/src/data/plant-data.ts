6ac16c6f75869b6cf909c81584bdd4259ca37e64
emilyy12 <emlyyang276@gmail.com>
2026-02-28 19:30:26 -0800

    scrollable tasks in dashboard

--- react-flask-app/frontend/src/data/plant-data.ts
+++ react-flask-app/frontend/src/data/plant-data.ts
@@ -27,45 +27,45 @@ export class PlantData {
     }
 
     private generateTasks(): Task[] {
-    const tasks: Task[] = [];
+        const tasks: Task[] = [];
 
-    if (this.soilMoisture < 20) {
-      tasks.push({
-        id: `${this.plantId}-water`,
-        plantId: this.plantId,
-        title: "moisture",
-        status: "pending"
-      });
-    }
+        if (this.soilMoisture < 20) {
+        tasks.push({
+            id: `${this.plantId}-water`,
+            plantId: this.plantId,
+            title: "moisture",
+            status: "pending"
+        });
+        }
 
-    if (this.airTempF > 90) {
-      tasks.push({
-        id: `${this.plantId}-temperature`,
-        plantId: this.plantId,
-        title: "temperature",
-        status: "pending"
-      });
-    }
+        if (this.airTempF > 90) {
+        tasks.push({
+            id: `${this.plantId}-temperature`,
+            plantId: this.plantId,
+            title: "temperature",
+            status: "pending"
+        });
+        }
 
-    if (this.humidity > 70) { // todo
-      tasks.push({
-        id: `${this.plantId}-humidity`,
-        plantId: this.plantId,
-        title: "humidity",
-        status: "pending"
-      });
-    }
+        if (this.humidity > 70) { // todo
+        tasks.push({
+            id: `${this.plantId}-humidity`,
+            plantId: this.plantId,
+            title: "humidity",
+            status: "pending"
+        });
+        }
 
-    if (this.lightLevel > 600) { // todo
-      tasks.push({
-        id: `${this.plantId}-light`,
-        plantId: this.plantId,
-        title: "light",
-        status: "pending"
-      });
-    }
+        if (this.lightLevel > 600) { // todo
+        tasks.push({
+            id: `${this.plantId}-light`,
+            plantId: this.plantId,
+            title: "light",
+            status: "pending"
+        });
+        }
 
-    return tasks;
+        return tasks;
 
     }
 }
